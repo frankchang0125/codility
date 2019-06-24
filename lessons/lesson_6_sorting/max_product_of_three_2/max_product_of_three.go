b2fe3cbb86da5035c3d640bdd818187070b1493d
Frank Chang <frankchang0125@gmail.com>
2019-06-24 21:04:58 +0800

    [Lesson 6] Sorting: MaxProductOfThree (Painless)

---
+++ lessons/lesson_6_sorting/max_product_of_three_2/max_product_of_three.go
@@ -0,0 +1,85 @@
+package maxproductofthree
+
+import (
+	"math"
+)
+
+/*
+ * A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+ *
+ * For example, array A such that:
+ *
+ *   A[0] = -3
+ *   A[1] = 1
+ *   A[2] = 2
+ *   A[3] = -2
+ *   A[4] = 5
+ *   A[5] = 6
+ * contains the following example triplets:
+ *
+ * (0, 1, 2), product is −3 * 1 * 2 = −6
+ * (1, 2, 4), product is 1 * 2 * 5 = 10
+ * (2, 4, 5), product is 2 * 5 * 6 = 60
+ * Your goal is to find the maximal product of any triplet.
+ *
+ * Write a function:
+ *
+ * func Solution(A []int) int
+ *
+ * that, given a non-empty array A, returns the value of the maximal product of any triplet.
+ *
+ * For example, given array A such that:
+ *
+ *   A[0] = -3
+ *   A[1] = 1
+ *   A[2] = 2
+ *   A[3] = -2
+ *   A[4] = 5
+ *   A[5] = 6
+ * the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+ *
+ * Write an efficient algorithm for the following assumptions:
+ *
+ * N is an integer within the range [3..100,000];
+ * each element of array A is an integer within the range [−1,000..1,000].
+ */
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+//
+// Reference: http://bit.ly/2Y9dBI5
+func Solution(A []int) int {
+	max1 := math.MinInt32
+	max2 := math.MinInt32
+	max3 := math.MinInt32
+	min1 := math.MaxInt32
+	min2 := math.MaxInt32
+
+	for _, num := range A {
+		if num > max1 {
+			max3 = max2
+			max2 = max1
+			max1 = num
+		} else if num > max2 {
+			max3 = max2
+			max2 = max1
+		} else if num > max3 {
+			max3 = num
+		}
+
+		if num < min1 {
+			min2 = min1
+			min1 = num
+		} else if num < min2 {
+			min2 = num
+		}
+	}
+
+	return max(max1*max2*max3, min1*min2*max1)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
